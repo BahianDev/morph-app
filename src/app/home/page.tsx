f86649d012b14396bb757c151e28ae1be305fdd2
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2025-05-02 14:03:56 -0300

    fix: remove morphy container from home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 items-center justify-center">
         <div className="flex space-x-5 flex-col lg:flex-row items-center">
-          <Link href="/home" className="flex flex-col items-center w-full">
+          {/* <Link href="/home" className="flex flex-col items-center w-full">
             <div className="relative bg-custom-gray hover:bg-primary transition-all duration-1000 p-4 rounded-xl cursor-pointer">
               <img src="/home/morphy.png" alt="MORPH" />
               <div className="absolute inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center rounded-xl">
@@ -13,8 +13,8 @@ export default function Home() {
               </div>
             </div>
             <span className="font-bold text-3xl">MORPH A MORPHY</span>
-          </Link>
-          <Link href="/memes" className="flex flex-col items-center w-full">
+          </Link> */}
+          <Link href="/memes" className="flex flex-col items-center w-96">
             <div className="bg-custom-gray hover:bg-primary transition-all duration-1000 p-4 rounded-xl cursor-pointer">
               <img  src="/home/memes.png" alt="MORPH" />
             </div>
