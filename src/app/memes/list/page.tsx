65d84edf3efd3f48258d175a813bdab984231c81
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2025-06-11 09:55:24 -0300

    fix: list memes

--- src/app/memes/list/page.tsx
+++ src/app/memes/list/page.tsx
@@ -11,20 +11,20 @@ import { MEME_CONTRACT_ADDRESS } from "@/constants";
 
 export default function MemesList() {
   const { address } = useAccount();
-
   const [nfts, setNfts] = useState<any[]>([]);
 
   const getNfts = useCallback(async () => {
     if (!address) return;
-  
+
     setNfts([]);
-  
+
     const supply = await readContract(config, {
       abi,
       address: MEME_CONTRACT_ADDRESS,
       functionName: "totalSupply",
     }).then((r) => Number(r));
-  
+
+    const loaded: any[] = [];
     for (let index = 0; index < supply; index++) {
       const tokenId = index + 1;
       const uri = await readContract(config, {
@@ -33,36 +33,45 @@ export default function MemesList() {
         functionName: "tokenURI",
         args: [tokenId],
       }).then(String);
-  
+
       const { data: metadata }: any = await axios.get(uri);
-  
-      // adiciona o tokenId no objeto antes de setar
-      setNfts((prev) => [
-        ...prev,
-        {
-          tokenId,
-          ...metadata,
-        },
-      ]);
+
+      // Quebra cache adicionando timestamp
+      const cacheBustedImage = `${metadata.image}?ts=${Date.now()}`;
+
+      loaded.push({
+        tokenId,
+        ...metadata,
+        image: cacheBustedImage,
+      });
     }
+
+    setNfts(loaded);
   }, [address]);
-  
 
   useEffect(() => {
     getNfts();
   }, [address, getNfts]);
 
+
+  console.log(nfts)
+
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-20">
       <div className="flex flex-col items-center justify-center gap-6 bg-[rgb(217,217,217)] p-5 rounded-lg">
-        <h1 className="text-7xl text-white font-bold">
+        <h1 className="text-5xl md:text-7xl text-white font-bold">
           <strong className="text-primary">MORPH’D</strong> MEMES
         </h1>
-        <div className="flex flex-wrap w-full items-center justify-center gap-6 ">
-          {nfts &&
-            nfts.length > 0 &&
-            nfts.map((nft, key) => (
-              <Card key={key} attributes={nft.attributes} image={nft.image} name={nft.name} id={nft.tokenId} />
+        <div className="flex flex-wrap w-full items-center justify-center gap-6">
+          {nfts.length > 0 &&
+            nfts.map((nft) => (
+              <Card
+                key={nft.tokenId}
+                attributes={nft.attributes}
+                image={nft.image}
+                name={nft.name}
+                id={nft.tokenId}
+              />
             ))}
         </div>
       </div>
